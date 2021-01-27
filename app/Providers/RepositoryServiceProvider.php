6a4b1d9b11626a89830914386774caabb7f0c896
Marcelo Vìllares <celovillares@gmail.com>
2021-01-27 14:21:15 -0300

    Api aula 19-10

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Providers;
+
+
+use App\Repositories\Contracts\{
+    CategoryRepositoryInterface,
+    TenantRepositoryInterface
+};
+
+use App\Repositories\{
+    CategoryRepository,
+    TenantRepository
+};
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            TenantRepositoryInterface::class,
+            TenantRepository::class
+        );
+
+        $this->app->bind(
+            CategoryRepositoryInterface::class,
+            CategoryRepository::class
+        );
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->bind(
+            TenantRepositoryInterface::class,
+            TenantRepository::class
+        );
+    }
+}
--- app/Services/TenantService.php
+++ app/Services/TenantService.php
@@ -4,11 +4,26 @@ namespace App\Services;
 
 
 use App\Models\Plan;
+use App\Repositories\Contracts\TenantRepositoryInterface;
 
 class TenantService
 {
+    private $plan, $data = [];    
+    private $repository;
 
-    private $plan, $data = [];
+    public function __construct(TenantRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function getAllTenants(int $per_page){
+        return $this->repository->getAllTenants($per_page);
+    }
+
+    public function getTenantByUuid(string $uuid)
+    {
+        return $this->repository->getTenantByUuid($uuid);
+    }
 
     public function make(Plan $plan, array $data)
     {
